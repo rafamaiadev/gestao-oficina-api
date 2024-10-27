7259d8bbdfc7ac51208e5f544965f949e2651db6
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2024-10-27 16:21:52 -0300

    criação do projeto.

---
+++ src/main/java/br/com/gestao_oficina_api/GestaoOficinaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestao_oficina_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoOficinaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoOficinaApiApplication.class, args);
+	}
+
+}
