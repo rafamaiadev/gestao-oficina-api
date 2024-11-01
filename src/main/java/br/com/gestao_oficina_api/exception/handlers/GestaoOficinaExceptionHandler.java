e9a7a5ddfdc487e1f0414071f9e1749949417ce5
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2024-11-01 18:58:45 -0300

    feat implementado um tratamento para argumentos inválidos na requisição.

--- src/main/java/br/com/gestao_oficina_api/exception/handlers/GestaoOficinaExceptionHandler.java
+++ src/main/java/br/com/gestao_oficina_api/exception/handlers/GestaoOficinaExceptionHandler.java
@@ -5,8 +5,11 @@ import br.com.gestao_oficina_api.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GestaoOficinaExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -19,5 +22,17 @@ public class GestaoOficinaExceptionHandler {
                 request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> handleResourceNotFoundException(HttpServletRequest request) {
+        StandardError error = new StandardError(
+                System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                "Erro de validação",
+                "Um ou mais campos do formulário são inválidos.",
+                request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
